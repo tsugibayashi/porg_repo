3139ccac4e585e7ad1dfe214be692bd367dbfc54
Takashi Sugibayashi <sugibayashi@protonmail.com>
2022-05-04 19:00:15 +0900

    Added dwm_statusbar

---
+++ dwm_statusbar/install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+# Description: A dwm statusbar implemented in Python
+# URL:         https://github.com/tsugibayashi/dwm_statusbar.py
+# Depends on:  dwm x11-xserver-utils python3-psutil python3-mpd
+
+### variables ###
+name=dwm_statusbar
+version=1
+URL1=https://github.com/tsugibayashi/${name}.py.git
+
+### main routine ###
+if [ -d $name-$version ]; then
+    rm -rf $name-$version
+fi
+if [ ! -d $name-$version ]; then
+    git clone $URL1 $name-$version
+fi
+
+### install
+cd $name-$version
+sudo porg -lp $name-$version 'install -m755 dwm_statusbar.py /usr/bin/dwm_statusbar.py'
+
