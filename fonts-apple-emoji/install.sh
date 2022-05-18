ba0092ac1241dd053d63b5a5a05d591098610f51
Takashi Sugibayashi <sugibayashi@protonmail.com>
2022-05-18 13:22:35 +0900

    Added fonts-apple-emoji

---
+++ fonts-apple-emoji/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Description: Apple Color Emoji is a color typeface used by iOS and macOS to display emoji
+# URL: https://github.com/samuelngs/apple-emoji-linux
+# Depends on:
+
+### variables ###
+name=fonts-apple-emoji
+version=15.4
+source=AppleColorEmoji.ttf
+URL=https://github.com/samuelngs/apple-emoji-linux/releases/download/ios-${version}/${source}
+
+### main routine ###
+if [ ! -f $source ]; then
+    wget $URL
+fi
+
+# install
+sudo porg -lp $name-$version 'install -d -m 755 /usr/share/fonts/truetype/apple-emoji/'
+sudo porg -lp+ $name-$version 'cp -p *.ttf /usr/share/fonts/truetype/apple-emoji/'
+
+sudo fc-cache /usr/share/fonts/truetype/apple-emoji/
+#sudo mkfontscale /usr/share/fonts/truetype/apple-emoji/
+#sudo mkfontdir /usr/share/fonts/truetype/apple-emoji/
+
