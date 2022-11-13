fa5150d7a86dd8411608ec067537ed4c0798563f
Takashi Sugibayashi <sugibayashi@protonmail.com>
2022-11-13 10:50:23 +0900

    Modified font name

--- st/config.h
+++ st/config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 /*static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";*/
-static char *font = "VL Gothic:pixelsize=22:antialias=true:autohint=true";
+static char *font = "VLGothic:pixelsize=22:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
