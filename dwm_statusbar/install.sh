72ef9689b310738ce4abd96591df4ffcac58dce7
Takashi Sugibayashi <sugibayashi@protonmail.com>
2024-01-06 11:39:40 +0900

    Modified dwm_statusbar/install.sh

--- dwm_statusbar/install.sh
+++ dwm_statusbar/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 # Description: A dwm statusbar implemented in Python
 # URL:         https://github.com/tsugibayashi/dwm_statusbar.py
-# Depends on:  xsetroot dwm x11-xserver-utils python3-psutil python3-mpd
+# Depends on:  xsetroot dwm x11-xserver-utils python3-psutil python3-mpd python3-alsaaudio
 
 ### variables ###
 name=dwm_statusbar
