01aaae4782d02b9275756a7df51e3b0692d0c6e0
Takashi Sugibayashi <sugibayashi@protonmail.com>
2024-03-20 17:31:43 +0900

    Updated dwm from 6.4 to 6.5.

--- dwm/install.sh
+++ dwm/install.sh
@@ -5,36 +5,36 @@
 
 ### variables ###
 name=dwm
-version=6.4
+version=6.5
 source1=$name-$version.tar.gz
-source2=dwm.desktop
-patch1=dwm-systray-$version.diff
-config1=config.h
 URL_source1=http://dl.suckless.org/dwm/$source1
-URL_patch1=https://dwm.suckless.org/patches/systray/$patch1
+source2=dwm.desktop
+patch1=dwm-bottomgaps-6.4.diff
+config1=config.h
 
 ### main routine ###
 if [ ! -f $source1 ]; then
-	wget $URL_source1
+    wget $URL_source1
 fi
 if [ ! -f $patch1 ]; then
-	wget $URL_patch1
+    echo '[Error] not found '$patch1
+    exit 1
 fi
 if [ ! -f $source2 ]; then
-	echo '[Error] not found '$source2
-	exit 1
+    echo '[Error] not found '$source2
+    exit 1
 fi
 if [ ! -f $config1 ]; then
-	echo '[Error] not found '$config1
-	exit 1
+    echo '[Error] not found '$config1
+    exit 1
 fi
 
 if [ -d $name-$version ]; then
-	rm -rf $name-$version
+    rm -rf $name-$version
 fi
 tar zxvf $source1
 
-# patch dwm-systray
+# patch to dwm
 cd $name-$version
 patch -p1 < ../$patch1
 
